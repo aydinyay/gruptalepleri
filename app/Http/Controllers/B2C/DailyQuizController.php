cbaa7c8d811f23753ca3b94e7043682f9a425cf3
Aydın Yaylacıklılar <aydinyay@gmail.com>
2026-04-20 13:01:02 +0300

    debug: DailyQuiz hata mesajını response ve log'a yaz

--- app/Http/Controllers/B2C/DailyQuizController.php
+++ app/Http/Controllers/B2C/DailyQuizController.php
@@ -9,10 +9,15 @@ class DailyQuizController extends Controller
 {
     public function show()
     {
-        $quiz = (new DailyQuizService())->getToday();
+        try {
+            $quiz = (new DailyQuizService())->getToday();
+        } catch (\Throwable $e) {
+            \Illuminate\Support\Facades\Log::error('DailyQuiz fatal: ' . $e->getMessage() . ' ' . $e->getFile() . ':' . $e->getLine());
+            return response()->json(['error' => true, 'msg' => $e->getMessage()], 503);
+        }
 
         if (! $quiz) {
-            return response()->json(['error' => true], 503);
+            return response()->json(['error' => true, 'msg' => 'null result'], 503);
         }
 
         return response()->json($quiz);
